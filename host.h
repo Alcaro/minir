ef0de25abed7e4203dce3d405e12832f2db1eae9
Alcaro <floating@muncher.se>
2014-05-29 01:06:32 +0200

    Fix up this one. It's no problem for others to configure host.h, but I kinda want to be able to use this repo for myself too, preferably without breaking it for Windows users...

--- host.h
+++ host.h
@@ -1,5 +1,54 @@
 //This is configured for Windows; the configure script will configure it for Linux.
 
+#if (!defined(WINDOW_WIN32) && !defined(WINDOW_GTK3))
+#define WINDOW_GTK3
+#endif
+
+#ifdef FRONT_GTK3
+//Which window protocol is in use. Only select one. Some IO drivers work only on some protocols.
+//This refers to the deepest underlying API; for example X11, Wayland or Win32.
+#define WNDPROT_X11
+//#define WNDPROT_WINDOWS
+
+//Which window toolkit to use. Pick exactly one.
+//This refers to the top-level API that draws the widgets. Can be Qt, GTK+, Win32, or similar.
+#define WINDOW_GTK3
+//#define WINDOW_WIN32
+
+//Which system to use for file paths.
+#define FILEPATH_POSIX
+//#define FILEPATH_WINDOWS
+
+//#define LINEBREAK_CRLF
+
+//How to load dynamic libraries. Pick exactly one.
+#define DYLIB_POSIX
+//#define DYLIB_WIN32
+
+#define THREAD_PTHREAD
+//#define THREAD_WIN32
+
+//Which input/video/audio drivers to enable. Multiple of each kind may be enabled.
+#define INPUT_X11_XINPUT2
+//#define INPUT_RAWINPUT
+#define INPUT_X11
+//#define INPUT_DIRECTINPUT
+
+//#define VIDEO_D3D9
+#define VIDEO_OPENGL
+//#define VIDEO_GDI
+#define VIDEO_XSHM
+
+#define AUDIO_PULSEAUDIO
+//#define AUDIO_DIRECTSOUND
+
+//#define NO_ANON_UNION_STRUCT
+#define HAVE_ASPRINTF
+//#define NO_UNALIGNED_MEM
+#endif
+
+
+#ifdef WINDOW_WIN32
 //Which window protocol is in use. Only select one. Some IO drivers work only on some protocols.
 //#define WNDPROT_X11
 #define WNDPROT_WINDOWS
@@ -38,7 +87,12 @@
 //#define NO_ANON_UNION_STRUCT
 //#define HAVE_ASPRINTF
 //#define NO_UNALIGNED_MEM
+#endif
 
-//#define NEED_ICON_PNG
+#ifdef WINDOW_GTK3
+#define NEED_ICON_PNG
+#endif
 
+#ifdef WINDOW_WIN32
 #define NEED_MANUAL_LAYOUT
+#endif
