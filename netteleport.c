4481a8d2d82a518f98181398ccf07c2c8b1ec436
Alcaro <floating@muncher.se>
2014-08-07 01:13:32 +0200

    Dummy file so I know what to do later.

---
+++ netteleport.c
@@ -0,0 +1,11 @@
+//Dummy file, just contains design plans.
+
+//netteleport is a module to simulate a bad internet connection over localhost.
+//Features:
+// Can delay each packet for a set duration.
+// Can delay packets for random durations, including rearranging them.
+// Can randomly drop packets.
+// Can randomly duplicate packets, and use different delays for the copies.
+// Can drop all packets sent within randomly chosen intervals.
+// Can delay all packets sent within randomly chosen intervals, then send them all at once. In random orders, too.
+// Can enable or disable any of these features dynamically.
