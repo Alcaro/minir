12954dd25641f3729e5c010381dcf5c7a0101007
Alcaro <floating@muncher.se>
2014-08-14 15:09:30 +0200

    Mess around with this a bit.

--- tests/memdebug.c
+++ tests/memdebug.c
@@ -7,15 +7,22 @@
 #define _GNU_SOURCE
 #include <dlfcn.h>
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 static void* (*malloc_)(size_t);
-static void* (*realloc_)(void*, size_t);
 static void (*free_)(void*);
+static void* (*realloc_)(void*, size_t);
 static void* (*dlopen_)(const char *, int);
+static int (*dlclose_)(void*);
 
 struct memdebug {
 	void (*malloc)(void* ptr, size_t size);
-	void (*realloc)(void* prev, void* ptr, size_t size);
 	void (*free)(void* prev);
+	void (*realloc)(void* prev, void* ptr, size_t size);
+	
+	void* (*s_malloc)(size_t size);
+	void (*s_free)(void* prev);
+	void* (*s_realloc)(void* prev, size_t size);
 };
 static struct memdebug cb;
 static unsigned int ignore;
@@ -24,19 +31,56 @@ void memdebug_init(struct memdebug * i)
 {
 	ignore=0;
 	malloc_ = dlsym(RTLD_NEXT, "malloc");
-	realloc_ = dlsym(RTLD_NEXT, "realloc");
 	free_ = dlsym(RTLD_NEXT, "free");
+	realloc_ = dlsym(RTLD_NEXT, "realloc");
 	dlopen_ = dlsym(RTLD_NEXT, "dlopen");
+	dlclose_ = dlsym(RTLD_NEXT, "dlclose");
 	memcpy(&cb, i, sizeof(struct memdebug));
+	i->s_malloc=malloc_;
+	i->s_free=free_;
+	i->s_realloc=realloc_;
 }
 
 void* malloc(size_t size)
 {
 	void* ret=malloc_(size);
+	if (!ret) abort();
 	if (ignore==0)
 	{
 		ignore++;
 		cb.malloc(ret, size);
+		ignore--;
+	}
+	return ret;
+}
+
+void free(void* ptr)
+{
+	if (!ptr) return;
+	if (ignore==0)
+	{
+		ignore++;
+		cb.free(ptr);
+		ignore--;
+	}
+	free_(ptr);
+}
+
+void* realloc(void* ptr, size_t size)
+{
+	if (!ptr) return malloc(size);
+	if (!size)
+	{
+		free(ptr);
+		return NULL;
+	}
+	
+	void* ret=realloc_(ptr, size);
+	if (!ret) abort();
+	if (ignore==0)
+	{
+		ignore++;
+		cb.realloc(ptr, ret, size);
 		ignore--;
 	}
 	return ret;
@@ -50,41 +94,89 @@ void* calloc(size_t n, size_t size)
 	return ret;
 }
 
-void* realloc(void* ptr, size_t size)
-{
-	void* ret=realloc_(ptr, size);
-	if (ignore==0)
-	{
-		ignore++;
-		cb.realloc(ptr, ret, size);
-		ignore--;
-	}
-	return ret;
-}
+struct dl_map {
+	struct dl_map * next;
+	void * ptr;
+};
 
-void free(void* ptr)
-{
-	if (ignore==0)
-	{
-		ignore++;
-		cb.free(ptr);
-		ignore--;
-	}
-	free_(ptr);
-}
+struct dl_list {
+	struct dl_list * next;
+	void * handle;
+	struct dl_map * maps;
+};
+
+struct dl_list dl_head;
+struct dl_list * dl_tail=&dl_head;
 
 void* dlopen(const char * filename, int flag)
 {
+	ignore++;
 	void* ret=dlopen_(filename, flag);
 	if (ret)
 	{
-printf("dl=%s\n",filename);
-		ignore++;
-		char data[1024];
-		ignore--;
-printf("dl=%s\n",filename);
+		dl_tail->next=malloc(sizeof(struct dl_list));
+		dl_tail=dl_tail->next;
+		dl_tail->handle=ret;
+		dl_tail->maps=malloc(sizeof(struct dl_map));
+		struct dl_map * map=dl_tail->maps;
+		map->next=NULL;
+		
+//printf("dl=%s\n",filename);
+		FILE * f=fopen("/proc/self/maps", "rt");
+		char* lastend=NULL;
+		while (!feof(f))
+		{
+			char data[1024];
+			fgets(data, 1024, f);
+			bool thislib=false;
+			if (strstr(data, filename)) thislib=true;//assume that no libraries with the same name can be loaded
+			
+			char* start;
+			char* end;
+			char mode[5];//rwxp
+			sscanf(data, "%p-%p %s", &start, &end, mode);
+			if (mode[1]=='w' && (thislib || (start==lastend && strstr(data, "00000000 00:00 0"))))//the ugly condition catches BSS sections
+			{
+				cb.malloc(start, end-start);
+				
+				map->next=malloc(sizeof(struct dl_map));
+				map=map->next;
+				map->next=NULL;
+				map->ptr=start;
+			}
+			if (thislib) lastend=end;
+			else lastend=NULL;
+		}
+		fclose(f);
 	}
+	ignore--;
 	return ret;
+}
+
+int dlclose(void* handle)
+{
+	ignore++;
+	
+	struct dl_list * list=&dl_head;
+	while (list->next->handle != handle) list=list->next;
+	struct dl_list * listnext=list->next;
+	list->next=list->next->next;
+	if (dl_tail==listnext) dl_tail=list;
+	list=listnext;
+	
+	struct dl_map * map=list->maps->next;
+	while (map)
+	{
+		struct dl_map * next=map->next;
+		cb.free(map->ptr);
+		free(map);
+		map=next;
+	}
+	free(list->maps);
+	free(list);
+	
+	dlclose_(handle);
+	ignore--;
 }
 #endif
 
--- tests/retrostateverify.c
+++ tests/retrostateverify.c
@@ -10,41 +10,76 @@
 #include <time.h>
 /*
 gcc -I. -std=c99 tests/retrostateverify.c tests/memdebug.c libretro.c dylib.c memory.c -ldl -lrt -DDYLIB_POSIX -DWINDOW_MINIMAL window-none.c -Os -s -o retrostate
-./retrostate roms/testcore_libretro.so - 1
+./retrostate roms/testcore_libretro.so - 60
 
 won't work on Windows
 */
 
 //These three will be called for every malloc/etc done in the program.
-//dlopen will send the BSS and DATA segments to the malloc handler. calloc will also call malloc.
+//dlopen will send the DATA and BSS segments to the malloc handler. calloc will also call malloc.
+//realloc is guaranteed to only be done for reallocations. If either 'ptr' or 'size' are NULL/0, malloc or free will be called instead.
+//Within these callbacks, further malloc/etc will not recurse.
+//The s_ versions are set by memdebug_init; they act like normal malloc, but do not call the callbacks.
 struct memdebug {
 	void (*malloc)(void* ptr, size_t size);
-	void (*realloc)(void* prev, void* ptr, size_t size);
 	void (*free)(void* prev);
+	void (*realloc)(void* prev, void* ptr, size_t size);
+	
+	void* (*s_malloc)(size_t size);
+	void (*s_free)(void* prev);
+	void* (*s_realloc)(void* prev, size_t size);
 };
 void memdebug_init(struct memdebug * i);
 
 
-char bss[1024*1024];
-char data[1024*1024]={1};
-const char cdata[1024*1024]={1};
+struct meminf {
+	struct meminf * next;
+	uint8_t * ptr;
+	uint8_t * changing;
+	uint8_t * laststate;
+	size_t size;
+	size_t alloc_id;
+};
+static struct meminf mem;
+static struct meminf * tail;
+static size_t alloc_id=0;
 
-const char * context;
+static const char * context;
 
 static void tr_malloc(void* ptr, size_t size)
 {
-	printf("al %lu from %s\n", size, context);
-}
-
-static void tr_realloc(void* prev, void* ptr, size_t size)
-{
-	
+printf("al %lu from %s -> %p\n", size, context, ptr);
+	tail->next=malloc(sizeof(struct meminf));
+	tail=tail->next;
+	tail->next=NULL;
+	tail->ptr=ptr;
+	tail->changing=calloc(size,1);
+	tail->laststate=malloc(size);
+	tail->size=size;
+	tail->alloc_id=alloc_id++;
 }
 
 static void tr_free(void* prev)
 {
+printf("fr %p from %s\n", prev, context);
+	struct meminf * inf=&mem;
+	while (inf->next->ptr != prev) inf=inf->next;
+	struct meminf * infnext=inf->next;
+	inf->next=inf->next->next;
+	if (tail==infnext) tail=inf;
+	inf=infnext;
 	
+	free(infnext->changing);
+	free(infnext->laststate);
+	free(infnext);
 }
+
+static void tr_realloc(void* prev, void* ptr, size_t size)
+{
+	tr_free(prev);
+	tr_malloc(ptr, size);
+}
+
 
 void no_video(struct video * this, unsigned int width, unsigned int height, const void * data, unsigned int pitch) {}
 void no_audio(struct audio * this, unsigned int numframes, const int16_t * samples) {}
@@ -62,9 +97,10 @@ int randr(int lower, int upper)
 
 int main(int argc, char * argv[])
 {
-	struct memdebug i={ tr_malloc, tr_realloc, tr_free };
+	struct memdebug i={ tr_malloc, tr_free, tr_realloc };
 	memdebug_init(&i);
 	context="main";
+	tail=&mem;
 //printf("bss=%p-%p data=%p-%p cdata=%p-%p\n",bss,bss+sizeof(bss),data,data+sizeof(data),cdata,cdata+sizeof(cdata));
 //FILE* f=fopen("/proc/self/maps", "rt");
 //fread(data, 1,65536, f);
@@ -72,54 +108,42 @@ int main(int argc, char * argv[])
 //return 1;
 	
 	//window_init(&argc, &argv);
-	unsigned int iterations=atoi(argv[3]);
+	unsigned int frames=atoi(argv[3]);
 	
-	time_t starttime=time(NULL);
+	srand(time(NULL));
 	
-	for (unsigned int i=0;i<iterations;i++)
+	context="libretro_create";
+	struct libretro * core=libretro_create(argv[1], NULL, false);
+	if (!core)
 	{
-		printf("%u/%u\r", i, iterations);
-		fflush(stdout);
-		
-		srand(starttime);
-		srand(rand()+i);//this should generate multiple different random number streams, but allow me to rewind them
-		
-		context="libretro_create";
-		struct libretro * core=libretro_create(argv[1], NULL, false);
-		if (!core)
-		{
-			puts("Couldn't load core.");
-			return 1;
-		}
-		
-		static struct video novideo;
-		novideo.draw=no_video;
-		
-		static struct audio noaudio;
-		noaudio.render=no_audio;
-		
-		static struct libretroinput noinput;
-		noinput.query=rand_input;
-		
-		core->attach_interfaces(core, &novideo, &noaudio, &noinput);
-		
-		context="libretro->load_rom";
-		if (!core->load_rom(core, NULL,0, argv[2]))
-		{
-			puts("Couldn't load ROM.");
-			return 1;
-		}
-		
-		context="libretro->run";
-		
-		unsigned int frames=randr(30, 60);
-		
-		for (unsigned int i=0;i<frames;i++)
-		{
-			core->run(core);
-		}
-		
-		context="libretro->free";
-		core->free(core);
+		puts("Couldn't load core.");
+		return 1;
 	}
+	
+	static struct video novideo;
+	novideo.draw=no_video;
+	
+	static struct audio noaudio;
+	noaudio.render=no_audio;
+	
+	static struct libretroinput noinput;
+	noinput.query=rand_input;
+	
+	core->attach_interfaces(core, &novideo, &noaudio, &noinput);
+	
+	context="libretro->load_rom";
+	if (!core->load_rom(core, NULL,0, argv[2]))
+	{
+		puts("Couldn't load ROM.");
+		return 1;
+	}
+	
+	context="libretro->run";
+	for (unsigned int i=0;i<frames;i++)
+	{
+		core->run(core);
+	}
+	
+	context="libretro->free";
+	core->free(core);
 }
