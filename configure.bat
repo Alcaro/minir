9c959c1f455c773948ec01f988bfdf4c26368105
Alcaro <floating@muncher.se>
2014-08-29 17:13:17 +0200

    Clean up spacing a bit. (Or is that making a bigger mess than before?)

--- configure.bat
+++ configure.bat
@@ -1,11 +1,16 @@
-#Windows setup.
-echo WIN_LIB = -lgdi32 -lcomctl32 -lcomdlg32 -ldinput8 -ldxguid -lopengl32 -ldsound > Makefile.custom
-echo TRUE_CFLAGS = $(CFLAGS)                                                       >> Makefile.custom
-echo TRUE_CXXFLAGS = $(CXXFLAGS)                                                   >> Makefile.custom
-echo TRUE_LFLAGS = $(LFLAGS) $(WIN_LIB)                                            >> Makefile.custom
-echo EXESUFFIX = .exe                                                              >> Makefile.custom
-echo EXTRAOBJ = obj/resource$(OBJSUFFIX).o                                         >> Makefile.custom
-echo RC = windres                                                                  >> Makefile.custom
-echo RCFLAGS =                                                                     >> Makefile.custom
-echo obj/resource$(OBJSUFFIX).o: ico/*                                             >> Makefile.custom
-echo 	$(RC) $(RCFLAGS) ico/minir.rc obj/resource$(OBJSUFFIX).o                     >> Makefile.custom
+echo WIN_LIB = -lgdi32  > Makefile.custom
+echo WIN_LIB += -lcomctl32 >> Makefile.custom
+echo WIN_LIB += -lcomdlg32 >> Makefile.custom
+echo WIN_LIB += -ldinput8  >> Makefile.custom
+echo WIN_LIB += -ldxguid   >> Makefile.custom
+echo WIN_LIB += -lopengl32 >> Makefile.custom
+echo WIN_LIB += -lopengl32 >> Makefile.custom
+echo TRUE_CFLAGS = $(CFLAGS)               >> Makefile.custom
+echo TRUE_CXXFLAGS = $(CXXFLAGS)           >> Makefile.custom
+echo TRUE_LFLAGS = $(LFLAGS) $(WIN_LIB)    >> Makefile.custom
+echo EXESUFFIX = .exe                      >> Makefile.custom
+echo EXTRAOBJ = obj/resource$(OBJSUFFIX).o >> Makefile.custom
+echo RC = windres                          >> Makefile.custom
+echo RCFLAGS =                             >> Makefile.custom
+echo obj/resource$(OBJSUFFIX).o: ico/*     >> Makefile.custom
+echo 	$(RC) $(RCFLAGS) ico/minir.rc obj/resource$(OBJSUFFIX).o >> Makefile.custom
