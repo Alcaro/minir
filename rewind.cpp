47e4a01f052a5f4305d7e1b7f3f35b01380fb28f
Alcaro <floating@muncher.se>
2014-09-02 21:24:31 +0200

    Clean up this one a bit - out with all chars, we want only uint16 here.

--- rewind.cpp
+++ rewind.cpp
@@ -27,94 +27,10 @@
 //The start of the buffer contains a size pointing to the end of the buffer; the end points to its start.
 //Wrapping is handled by returning to the start of the buffer if the compressed data could potentially hit the edge;
 //if the compressed data could potentially overwrite the tail pointer, the tail retreats until it can no longer collide.
-//This means that on average, ~2*maxcompsize is unused at any given moment.
+//This means that on average, ~2*maxcompsize is unused at any given moment, assuming the object is full.
 
 #if __SSE2__
-#define PADDING 32//2*sizeof(__m128i)
-#else
-#define PADDING sizeof(size_t)
-#endif
-
-//TODO: replace all char* with uint16_t* or uint8_t*.
-struct rewindstack_impl {
-	struct rewindstack i;
-	
-	char * data;
-	size_t capacity;
-	char * head;//Reading and writing is done here.
-	char * tail;//If head comes close to this, discard a frame.
-	
-	char * thisblock;
-	char * nextblock;
-	bool thisblock_valid;
-	
-	size_t blocksize;//This one is rounded up from reset::blocksize.
-	size_t maxcompsize;//size_t+(blocksize+131071)/131072*(blocksize+u16+u16)+u16+u32+size_t (yes, the math is a bit ugly)
-	
-	unsigned int entries;
-};
-
-static void reset(struct rewindstack * this_, size_t blocksize, size_t capacity)
-{
-	struct rewindstack_impl * this=(struct rewindstack_impl*)this_;
-	
-	size_t newblocksize=((blocksize-1)|(sizeof(uint16_t)-1))+1;
-	if (this->blocksize!=newblocksize)
-	{
-		this->blocksize=newblocksize;
-		
-		size_t maxcblkcover=UINT16_MAX*sizeof(uint16_t);
-		size_t maxcblks=(this->blocksize+maxcblkcover-1)/maxcblkcover;
-		this->maxcompsize=this->blocksize + maxcblks*sizeof(uint16_t)*2 + sizeof(uint16_t)+sizeof(uint32_t) + sizeof(size_t)*2;
-		
-		free(this->thisblock);
-		free(this->nextblock);
-		
-		size_t nbytes=this->blocksize+sizeof(uint16_t)*4+PADDING;
-		this->thisblock=malloc(nbytes);
-		this->nextblock=malloc(nbytes);
-		memset(this->thisblock, 0, nbytes);
-		memset(this->nextblock, 0, nbytes);
-		//Force in a different byte at the end, so we don't need to check bounds in the innermost loop (it's expensive).
-		//There is also a large amount of data that's the same, to stop the other scan.
-		//There is also some padding at the end. This is so we don't read outside the buffer end if we're reading in large blocks;
-		// it doesn't make any difference to us, but sacrificing 16 bytes to get Valgrind happy is worth it.
-		*(uint16_t*)(this->thisblock+this->blocksize+sizeof(uint16_t)*3)=0xFFFF;
-		*(uint16_t*)(this->nextblock+this->blocksize+sizeof(uint16_t)*3)=0x0000;
-	}
-	
-	if (capacity!=this->capacity)
-	{
-		free(this->data);
-		this->data=malloc(capacity);
-		this->capacity=capacity;
-	}
-	
-	this->head=this->data+sizeof(size_t);
-	this->tail=this->data+sizeof(size_t);
-	
-	this->thisblock_valid=false;
-	
-	this->entries=0;
-}
-
-static void * push_begin(struct rewindstack * this_)
-{
-	struct rewindstack_impl * this=(struct rewindstack_impl*)this_;
-	//We need to ensure we have an uncompressed copy of the last pushed state, or we could
-	// end up applying a 'patch' to wrong savestate, and that'd blow up rather quickly.
-	if (!this->thisblock_valid)
-	{
-		if (this_->pull(this_))
-		{
-			this->thisblock_valid=true;
-			this->entries++;
-		}
-	}
-	return this->nextblock;
-}
-
-#if __SSE2__
+#define PADDING 16//2*sizeof(__m128i)/sizeof(size_t)
 	//The input can be anything in the range FFFF0001 to FFFFFFFF, inclusive. Other values, including 0, are known impossible.
 #if defined(__GNUC__)
 static inline int compat_ctz(unsigned int x)
@@ -170,7 +86,10 @@ static inline size_t find_change(const uint16_t * a, const uint16_t * b)
 		b128++;
 	}
 }
+
 #else
+
+#define PADDING sizeof(size_t)/sizeof(uint16_t)
 static inline size_t find_change(const uint16_t * a, const uint16_t * b)
 {
 	const uint16_t * a_org=a;
@@ -205,6 +124,7 @@ static inline size_t find_change(const uint16_t * a, const uint16_t * b)
 #endif
 
 static inline size_t find_same(const uint16_t * a, const uint16_t * b)
+//This one is less optimized than find_change, because far less is changed than unchanged.
 {
 	const uint16_t * a_org=a;
 #ifdef NO_UNALIGNED_MEM
@@ -219,6 +139,7 @@ static inline size_t find_same(const uint16_t * a, const uint16_t * b)
 		//With this, it's random whether two consecutive identical words are caught.
 		//Luckily, compression rate is the same for both cases, and three is always caught.
 		//(We prefer to miss two-word blocks, anyways; it gives fewer iterations of the outer loop, as well as in the decompressor.)
+		//We can not change it to size_t because that could (on 64bit) miss up to siz consecutive words.
 		const uint32_t* a_big=(const uint32_t*)a;
 		const uint32_t* b_big=(const uint32_t*)b;
 		
@@ -239,88 +160,179 @@ static inline size_t find_same(const uint16_t * a, const uint16_t * b)
 	return a-a_org;
 }
 
+size_t read_size(uint16_t* source)
+{
+#ifdef NO_UNALIGNED_MEM
+	size_t ret;
+	memcpy(ret, source, sizeof(size_t);
+	return ret;
+#else
+	return *(size_t*)source;
+#endif
+}
+
+void write_size(uint16_t* pos, size_t val)
+{
+#ifdef NO_UNALIGNED_MEM
+	memcpy(pos, val, sizeof(size_t);
+#else
+	*(size_t*)pos = val;
+#endif
+}
+
+//TODO: replace all char* with uint16_t* or uint8_t*.
+struct rewindstack_impl {
+	struct rewindstack i;
+	
+	uint16_t * data;
+	size_t capacity;
+	uint16_t * head;//Reading and writing is done here.
+	uint16_t * tail;//If head comes close to this, discard a frame.
+	
+	uint16_t * thisblock;
+	uint16_t * nextblock;
+	bool thisblock_valid;
+	
+	size_t blocksize;//This one is rounded up from reset::blocksize.
+	size_t maxcompsize;//size_t+(blocksize+131071)/131072*(blocksize+u16+u16)+u16+u32+size_t (yes, the math is a bit ugly)
+	
+	unsigned int entries;
+};
+
+static void reset(struct rewindstack * this_, size_t blocksize, size_t capacity)
+{
+	struct rewindstack_impl * this=(struct rewindstack_impl*)this_;
+	
+	size_t newblocksize=(blocksize+sizeof(uint16_t)-1)/sizeof(uint16_t);
+	if (this->blocksize!=newblocksize)
+	{
+		this->blocksize=newblocksize;
+		
+		size_t maxcblkcover=UINT16_MAX;
+		size_t maxcblks=(this->blocksize+maxcblkcover-1)/maxcblkcover;
+		this->maxcompsize=sizeof(size_t)/sizeof(uint16_t) + maxcblks*2 + this->blocksize + 1+2 + sizeof(size_t)/sizeof(uint16_t);
+		
+		free(this->thisblock);
+		free(this->nextblock);
+		
+		size_t nwords=this->blocksize+4+PADDING;
+		this->thisblock=malloc(nwords*sizeof(uint16_t));
+		this->nextblock=malloc(nwords*sizeof(uint16_t));
+		memset(this->thisblock, 0, nwords*sizeof(uint16_t));
+		memset(this->nextblock, 0, nwords*sizeof(uint16_t));
+		//Force in a different byte at the end, so we don't need to check bounds in the innermost loop (it's expensive).
+		//There is also a large amount of data that's the same, to stop the other scan.
+		//There is also some padding at the end. This is so we don't read outside the buffer end if we're reading in large blocks;
+		// it doesn't make any difference to us, but sacrificing 16 bytes to get Valgrind happy is worth it.
+		this->thisblock[this->blocksize+3]=0xFFFF;
+		this->nextblock[this->blocksize+3]=0x0000;
+	}
+	
+	capacity=(capacity+sizeof(uint16_t)-1)/sizeof(uint16_t);
+	if (capacity!=this->capacity)
+	{
+		free(this->data);
+		this->data=malloc(capacity*sizeof(uint16_t));
+		this->capacity=capacity;
+	}
+	
+	this->head=this->data+sizeof(size_t)/sizeof(uint16_t);
+	this->tail=this->data+sizeof(size_t)/sizeof(uint16_t);
+	
+	this->thisblock_valid=false;
+	
+	this->entries=0;
+}
+
+static void * push_begin(struct rewindstack * this_)
+{
+	struct rewindstack_impl * this=(struct rewindstack_impl*)this_;
+	//We need to ensure we have an uncompressed copy of the last pushed state, or we could
+	// end up applying a 'patch' to wrong savestate, and that'd blow up rather quickly.
+	if (!this->thisblock_valid)
+	{
+		if (this_->pull(this_))
+		{
+			this->thisblock_valid=true;
+			this->entries++;
+		}
+	}
+	return this->nextblock;
+}
+
 static void push_end(struct rewindstack * this_)
 {
 	struct rewindstack_impl * this=(struct rewindstack_impl*)this_;
 	if (this->thisblock_valid)
 	{
-		if (this->capacity<sizeof(size_t)+this->maxcompsize) return;
+		if (this->capacity<this->maxcompsize) return;
 		
 	recheckcapacity:;
-		size_t headpos=(this->head-this->data);
-		size_t tailpos=(this->tail-this->data);
+		size_t headpos=this->head-this->data;
+		size_t tailpos=this->tail-this->data;
 		size_t remaining=(tailpos+this->capacity-sizeof(size_t)-headpos-1)%this->capacity + 1;
 		if (remaining<=this->maxcompsize)
 		{
-			size_t tailpos;
-			memcpy(&tailpos, this->tail, sizeof(size_t));
-			this->tail=this->data + tailpos;
+			this->tail = this->data + read_size(this->tail);
 			this->entries--;
 			goto recheckcapacity;
 		}
 		
-		const char* olddat=this->thisblock;
-		const char* newdat=this->nextblock;
-		char* compressed=this->head+sizeof(size_t);
+		const uint16_t * olddat=this->thisblock;
+		const uint16_t * newdat=this->nextblock;
+		uint16_t* compressed=this->head+sizeof(size_t)/sizeof(uint16_t);
 		
 		//Begin compression code; 'compressed' will point to the end of the compressed data (excluding the prev pointer).
-		const uint16_t * old16=(const uint16_t*)olddat;
-		const uint16_t * new16=(const uint16_t*)newdat;
-		uint16_t * compressed16=(uint16_t*)compressed;
-		size_t num16s=this->blocksize/sizeof(uint16_t);
-		while (num16s)
+		size_t sizeleft=this->blocksize;
+		while (sizeleft)
 		{
-			size_t skip=find_change(old16, new16);
+			size_t skip=find_change(olddat, newdat);
 			//size_t skip=find_change_b(old16, new16);
 			//if (skip!=skip2) abort();
 			
-			if (skip>=num16s) break;
-			old16+=skip;
-			new16+=skip;
-			num16s-=skip;
+			if (skip>=sizeleft) break;
+			
+			//This will make it scan the entire thing again, but it only hits on 8GB unchanged
+			//data, and there's no way to handle 8GB data per frame.
+			if (skip>UINT32_MAX) skip=UINT32_MAX;
+			
+			olddat+=skip;
+			newdat+=skip;
+			sizeleft-=skip;
 			
 			if (skip>UINT16_MAX)
 			{
-				if (skip>UINT32_MAX)
-				{
-					//This will make it scan the entire thing again, but it only hits on 8GB unchanged
-					//data anyways, and if you're doing that, you've got bigger problems.
-					skip=UINT32_MAX;
-				}
-				*(compressed16++)=0;
-				*(compressed16++)=skip;
-				*(compressed16++)=skip>>16;
-				skip=0;
+				compressed[0]=0;
+				compressed[1]=skip;
+				compressed[2]=skip>>16;
+				compressed+=3;
 				continue;
 			}
 			
-			size_t changed=find_same(old16, new16);
+			size_t changed=find_same(olddat, newdat);
 			if (changed>UINT16_MAX) changed=UINT16_MAX;
-			*(compressed16++)=changed;
-			*(compressed16++)=skip;
-			for (unsigned int i=0;i<changed;i++) compressed16[i]=old16[i];
-			old16+=changed;
-			new16+=changed;
-			compressed16+=changed;
-			num16s-=changed;
+			compressed[0]=changed;
+			compressed[1]=skip;
+			compressed+=2;
+			for (unsigned int i=0;i<changed;i++) compressed[i]=olddat[i];
+			olddat+=changed;
+			newdat+=changed;
+			compressed+=changed;
+			sizeleft-=changed;
 		}
-		compressed16[0]=0;
-		compressed16[1]=0;
-		compressed16[2]=0;
-		compressed=(char*)(compressed16+3);
+		compressed[0]=0;
+		compressed[1]=0;
+		compressed[2]=0;
+		compressed+=3;
 		//End compression code.
 		
 		if (compressed-this->data+this->maxcompsize > this->capacity)
 		{
 			compressed=this->data;
-			if (this->tail==this->data+sizeof(size_t)) this->tail=this->data + *(size_t*)this->tail;
+			//if (this->tail==(MEOW*)((char*)this->data+sizeof(size_t))) this->tail=(MEOW*)((char*)this->data + *(size_t*)this->tail);
 		}
-		size_t tmp=this->head-this->data;
-		memcpy(compressed, &tmp, sizeof(size_t));
-		compressed+=sizeof(size_t);
-		
-		tmp=compressed-this->data;
-		memcpy(this->head, &tmp, sizeof(size_t));
+		write_size(compressed, this->head-this->data);
+		compressed+=sizeof(size_t)/sizeof(uint16_t);
 		this->head=compressed;
 	}
 	else
@@ -328,7 +340,7 @@ static void push_end(struct rewindstack * this_)
 		this->thisblock_valid=true;
 	}
 	
-	char * swap=this->thisblock;
+	uint16_t* swap=this->thisblock;
 	this->thisblock=this->nextblock;
 	this->nextblock=swap;
 	
@@ -354,35 +366,32 @@ static const void * pull(struct rewindstack * this_)
 	
 	if (this->head==this->tail) return NULL;
 	
-	size_t start;
-	memcpy(&start, this->head - sizeof(size_t), sizeof(size_t));
-	this->head=this->data+start;
+	size_t start=read_size(this->head - sizeof(size_t)/sizeof(uint16_t));
+	this->head=this->data + start;
 	
-	const char * compressed=this->data+start+sizeof(size_t);
-	char * out=this->thisblock;
+	const uint16_t * compressed = this->data+start+sizeof(size_t)/sizeof(uint16_t);
+	uint16_t * out=this->thisblock;
 	//Begin decompression code
 	//out is the last pushed (or returned) state
-	const uint16_t * compressed16=(const uint16_t*)compressed;
-	uint16_t * out16=(uint16_t*)out;
 	while (true)
 	{
-		uint16_t numchanged=*(compressed16++);
+		uint16_t numchanged=*(compressed++);
 		if (numchanged)
 		{
-			out16+=*(compressed16++);
+			out+=*(compressed++);
 			//We could do memcpy, but it seems that memcpy has a constant-per-call overhead that actually shows up.
 			//Our average size in here seems to be 8 or something.
 			//Therefore, we do something with lower overhead.
-			for (int i=0;i<numchanged;i++) out16[i]=compressed16[i];
-			compressed16+=numchanged;
-			out16+=numchanged;
+			for (int i=0;i<numchanged;i++) out[i]=compressed[i];
+			compressed+=numchanged;
+			out+=numchanged;
 		}
 		else
 		{
-			uint32_t numunchanged=compressed16[0] | compressed16[1]<<16;
+			uint32_t numunchanged=compressed[0] | compressed[1]<<16;
 			if (!numunchanged) break;
-			compressed16+=2;
-			out16+=numunchanged;
+			compressed+=2;
+			out+=numunchanged;
 		}
 	}
 	//End decompression code
@@ -396,8 +405,8 @@ static void capacity_f(struct rewindstack * this_, unsigned int * entries, size_
 {
 	struct rewindstack_impl * this=(struct rewindstack_impl*)this_;
 	
-	size_t headpos=(this->head-this->data);
-	size_t tailpos=(this->tail-this->data);
+	size_t headpos=(this->head-this->data)*sizeof(uint16_t);
+	size_t tailpos=((uint16_t*)this->tail-this->data)*sizeof(uint16_t);
 	size_t remaining=(tailpos+this->capacity-sizeof(size_t)-headpos-1)%this->capacity + 1;
 	
 	if (entries) *entries=this->entries;
