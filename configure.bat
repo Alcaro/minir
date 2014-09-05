da94b493cf783a89e8bb8480da13caa1953e9534
Alcaro <floating@muncher.se>
2014-09-05 17:54:18 +0200

    Rename Makefile.custom to config.mk because that's what Automake uses. I never liked the name Makefile.custom, anyways.

--- configure.bat
+++ configure.bat
@@ -1,16 +1,13 @@
-echo WIN_LIB = -lgdi32  > Makefile.custom
-echo WIN_LIB += -lcomctl32 >> Makefile.custom
-echo WIN_LIB += -lcomdlg32 >> Makefile.custom
-echo WIN_LIB += -ldinput8  >> Makefile.custom
-echo WIN_LIB += -ldxguid   >> Makefile.custom
-echo WIN_LIB += -lopengl32 >> Makefile.custom
-echo WIN_LIB += -lopengl32 >> Makefile.custom
-echo TRUE_CFLAGS = $(CFLAGS)               >> Makefile.custom
-echo TRUE_CXXFLAGS = $(CXXFLAGS)           >> Makefile.custom
-echo TRUE_LFLAGS = $(LFLAGS) $(WIN_LIB)    >> Makefile.custom
-echo EXESUFFIX = .exe                      >> Makefile.custom
-echo EXTRAOBJ = obj/resource$(OBJSUFFIX).o >> Makefile.custom
-echo RC = windres                          >> Makefile.custom
-echo RCFLAGS =                             >> Makefile.custom
-echo obj/resource$(OBJSUFFIX).o: ico/*     >> Makefile.custom
-echo 	$(RC) $(RCFLAGS) ico/minir.rc obj/resource$(OBJSUFFIX).o >> Makefile.custom
+echo WIN_LIB =  -lgdi32     > config.mk
+echo WIN_LIB += -lcomctl32 >> config.mk
+echo WIN_LIB += -lcomdlg32 >> config.mk
+echo WIN_LIB += -ldinput8  >> config.mk
+echo WIN_LIB += -ldxguid   >> config.mk
+echo WIN_LIB += -lopengl32 >> config.mk
+echo CONF_LFLAGS = $(WIN_LIB)              >> config.mk
+echo EXESUFFIX = .exe                      >> config.mk
+echo EXTRAOBJ = obj/resource$(OBJSUFFIX).o >> config.mk
+echo RC = windres                          >> config.mk
+echo RCFLAGS =                             >> config.mk
+echo obj/resource$(OBJSUFFIX).o: ico/*     >> config.mk
+echo 	$(RC) $(RCFLAGS) ico/minir.rc obj/resource$(OBJSUFFIX).o >> config.mk
