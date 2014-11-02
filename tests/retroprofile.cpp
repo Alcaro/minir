79db24d041ab12388ccb20305b3e01954d631bf3
Alcaro <floating@muncher.se>
2014-11-02 14:33:18 +0100

    Update retroprofile to the new video interface.

--- tests/retroprofile.cpp
+++ tests/retroprofile.cpp
@@ -1,20 +1,26 @@
+#if 0
+<<COMMENT1
 //retroprofile - a headless libretro frontend, for performance testing and PGO compilation
 //usage: retroprofile corepath rompath frames
 //example: ./retroprofile roms/gambatte_libretro.so roms/zeldaseasons.gbc 10000
+COMMENT1
+
+cd tests || true
+cd ..
+g++ -I. tests/retroprofile.cpp \
+	libretro.cpp dylib.cpp memory.cpp video.cpp \
+	-ldl -lrt -DDYLIB_POSIX -DWINDOW_MINIMAL window-none.cpp -Os -s -o retroprofile
+mv retroprofile ~/bin
+
+<<COMMENT2
+#endif
 
 #include "minir.h"
 #include <stdlib.h>
 #include <stdio.h>
-/*
-gcc -I. -std=c99 tests/retroprofile.c libretro.c dylib.c memory.c -ldl -lrt -DDYLIB_POSIX -DWINDOW_MINIMAL window-none.c -Os -s -o retroprofile
-mv retroprofile ~/bin
 
-compile differently on Windows
-*/
-
-void no_video(struct video * this, unsigned int width, unsigned int height, const void * data, unsigned int pitch) {}
-void no_audio(struct audio * this, unsigned int numframes, const int16_t * samples) {}
-int16_t no_input(struct libretroinput * this, unsigned port, unsigned device, unsigned index, unsigned id) { return 0; }
+void no_audio(struct audio * This, unsigned int numframes, const int16_t * samples) {}
+int16_t no_input(struct libretroinput * This, unsigned port, unsigned device, unsigned index, unsigned id) { return 0; }
 
 int main(int argc, char * argv[])
 {
@@ -29,8 +35,7 @@ int main(int argc, char * argv[])
 		return 1;
 	}
 	
-	static struct video novideo;
-	novideo.draw=no_video;
+	video* v=list_video[0]->create2d(0);
 	
 	static struct audio noaudio;
 	noaudio.render=no_audio;
@@ -38,7 +43,7 @@ int main(int argc, char * argv[])
 	static struct libretroinput noinput;
 	noinput.query=no_input;
 	
-	core->attach_interfaces(core, &novideo, &noaudio, &noinput);
+	core->attach_interfaces(core, v, &noaudio, &noinput);
 	
 	if (!core->load_rom(core, NULL,0, argv[2]))
 	{
@@ -73,3 +78,6 @@ int main(int argc, char * argv[])
 	printf("Time to exit: %luus\n", t_end-t_run_done);
 	printf("Total time: %luus\n", t_end-t_init);
 }
+#if 0
+COMMENT2
+#endif
