59bc61d2ee0d17cde69c4e714ffb98adfabb4385
Alcaro <floating@muncher.se>
2014-11-30 21:19:33 +0100

    Rewrite this region a bit.

--- window-native.cpp
+++ window-native.cpp
@@ -104,11 +104,17 @@ void _window_init_shared()
 	
 	cwd_init=getcwd(NULL, 0);
 	//try a couple of useless directories and hope one of them works
-	chdir("/");
-	chdir("/tmp");
-	chdir("/home");
-	chdir("/dev");
-	chdir("/dev/cpu");
+	//this seems to be the best one:
+	//- even root can't write to this one
+	//- it contains no files with a plausible name on a standard Ubuntu box (I have an ath9k-phy0, nothing will ever want)
+	//- a wild write will not do anything dangerous except turn on some lamps
+	!chdir("/sys/class/leds/") ||
+		//the rest are in case it's not accessible (weird chroot? not linux?), so try some random things
+		!chdir("/sys/") ||
+		!chdir("/dev/") ||
+		!chdir("/home/") ||
+		!chdir("/tmp/") ||
+		!chdir("/");
 	cwd_bogus=getcwd(NULL, 0);
 }
 
