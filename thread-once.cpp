c796e04c875761a7ff0b215fdb2d82c54a5c9fb6
Alcaro <floating@muncher.se>
2015-01-17 20:53:36 +0100

    Note to self - test stuff before pushing.

--- thread-once.cpp
+++ thread-once.cpp
@@ -1,12 +1,17 @@
 #include "os.h"
 
-mutex* thread_once_mutex(mutex* * location)
+template<typename T> T* thread_once_object(T* * location)
 {
 	//this is different from thread_once - the equivalent of calculate() can be called twice. If this happens, we undo one of them.
-	if (*location) return;//nonatomic - if something weird happens, all that happens is that another event is created and deleted.
-	event* ev=new event;
+	if (*location) return *location;//nonatomic - if something weird happens, all that happens is that another item is created and deleted.
+	T* ev=new T;
 	if (lock_write_eq((void**)location, NULL, ev)!=NULL) delete ev;
 	return *location;
+}
+
+mutex* thread_once_mutex(mutex* * location)
+{
+	return thread_once_object(location);
 }
 
 static event* contention_unlocker=NULL;
@@ -35,7 +40,7 @@ void* thread_once_core(void* * item, function<void*()> calculate)
 		// because the other threads know that they're only allowed to replace it with tag_contended.
 		if (lock_write_eq(item, tag_busy, result)==tag_contended)
 		{
-			thread_once_mutex(&contention_unlocker);
+			thread_once_object(&contention_unlocker);
 			lock_write(item, result);
 			contention_unlocker->signal();
 		}
@@ -44,7 +49,7 @@ void* thread_once_core(void* * item, function<void*()> calculate)
 	{
 		//don't bother optimizing this, contention only happens a few times during program lifetime
 		lock_write_eq(item, tag_busy, tag_contended);
-		thread_once_mutex(&contention_unlocker);
+		thread_once_object(&contention_unlocker);
 		while (lock_read(item)==tag_busy) contention_unlocker->wait();
 		contention_unlocker->signal();
 	}
