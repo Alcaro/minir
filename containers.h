5fb16bdc356aec03b73d0bb323ee20478f729f96
Alcaro <floating@muncher.se>
2015-03-08 22:01:31 +0100

    Shove this one into the header so it can be reused.

--- containers.h
+++ containers.h
@@ -387,7 +387,153 @@ public:
 	}
 };
 
-
-template<typename T> class slimintarray {
+template<typename T> class multinum {
+	enum { numinline = sizeof(T*) / sizeof(T) };
 	
+	void assertions() {
+		static_assert(numinline > 1);//there must be sufficient space for at least two ints in a pointer
+		static_assert(numinline * sizeof(T) == sizeof(T*));//the size of a pointer must be a multiple of the size of an int
+		static_assert((numinline & (numinline-1)) == 0);//this multiple must be a power of two
+		static_assert(numinline<<1 < (T)-1);//the int must be large enough to store how many ints fit in a pointer, plus the tag bit
+	};
+	
+	//the value is either:
+	//if the lowest bit of ptr_raw is set:
+	// the number of items is (ptr>>1) & numinline
+	// the others are in inlines_raw[], at position inlines()
+	//else:
+	// the number of items is in ptr[0]
+	// the items are in ptr[1..count]
+	union {
+		T* ptr_raw;
+		T inlines_raw[numinline];
+	};
+	
+	static int tag_offset()
+	{
+		union {
+			T* ptr;
+			T uint[numinline];
+		} u;
+		u.ptr = (T*)(uintptr_t)0xFFFF;
+		
+		if (u.uint[0]==0xFFFF) return 0; // little endian
+		else if (u.uint[numinline-1]==0xFF) return numinline-1; // big endian
+		else return -1; // middle endian - let's blow up (middle endian is dead, anyways)
+	}
+	
+	T& tag()
+	{
+		return inlines_raw[tag_offset()];
+	}
+	
+	bool is_inline()
+	{
+		return tag()&1;
+	}
+	
+	T* inlines()
+	{
+		if (tag_offset()==0) return inlines_raw+1;
+		else return inlines_raw;
+	}
+	
+public:
+	T* ptr()
+	{
+		if (is_inline()) return inlines();
+		else return ptr_raw+1;
+	}
+	
+	T count()
+	{
+		if (is_inline()) return tag()>>1;
+		else return ptr_raw[0];
+	}
+	
+private:
+	//If increased, does not initialize the new entries. If decreased, drops the top.
+	void set_count(T newcount)
+	{
+		T oldcount=count();
+		T* oldptr=ptr();
+		
+		if (oldcount < numinline && newcount < numinline)
+		{
+			tag() = newcount<<1 | 1;
+		}
+		if (oldcount >= numinline && newcount < numinline)
+		{
+			T* freethis = ptr_raw;
+			memcpy(inlines(), oldptr, sizeof(T)*newcount);
+			tag() = newcount<<1 | 1;
+			free(freethis);
+		}
+		if (oldcount < numinline && newcount >= numinline)
+		{
+			T* newptr = malloc(sizeof(T)*(1+newcount));
+			newptr[0] = newcount;
+			memcpy(newptr+1, oldptr, sizeof(T)*oldcount);
+			ptr_raw = newptr;
+		}
+		if (oldcount >= numinline && newcount >= numinline)
+		{
+			ptr_raw = realloc(ptr_raw, sizeof(T)*(1+newcount));
+			ptr_raw[0] = newcount;
+		}
+	}
+	
+public:
+	multinum()
+	{
+		tag() = 0<<1 | 1;
+	}
+	
+	void add(T val)
+	{
+		T* entries = ptr();
+		T num = count();
+		
+		for (T i=0;i<num;i++)
+		{
+			if (entries[i]==val) return;
+		}
+		
+		add_uniq(val);
+	}
+	
+	//Use this if the value is known to not exist in the set already.
+	void add_uniq(T val)
+	{
+		T num = count();
+		set_count(num+1);
+		ptr()[num] = val;
+	}
+	
+	void remove(T val)
+	{
+		T* entries = ptr();
+		T num = count();
+		
+		for (T i=0;i<num;i++)
+		{
+			if (entries[i]==val)
+			{
+				entries[i] = entries[num-1];
+				set_count(num-1);
+				break;
+			}
+		}
+	}
+	
+	T* get(T& len)
+	{
+		len=count();
+		return ptr();
+	}
+	
+	~multinum()
+	{
+		if (!is_inline()) free(ptr_raw);
+	}
 };
