b3d0111f05509882d8ad81e962b21ffcf8008ef3
Alcaro <floating@muncher.se>
2015-06-25 17:24:26 +0200

    Fix this to understand the latest headers.

--- subproj/retroprofile.cpp
+++ subproj/retroprofile.cpp
@@ -7,46 +7,43 @@ COMMENT1
 
 cd subproj || true
 cd ..
-g++ -I. subproj/retroprofile.cpp \
-	libretro.cpp dylib.cpp memory.cpp video.cpp \
-	-ldl -lrt -DDYLIB_POSIX -DWINDOW_MINIMAL window-none.cpp -Os -s -o retroprofile
-mv retroprofile ~/bin
+true -Os -s
+g++ -g subproj/retroprofile.cpp \
+	libretro.cpp dylib.cpp memory.cpp video.cpp file-posix.cpp -DFILEPATH_POSIX \
+	-ldl -lrt -DDYLIB_POSIX -DWINDOW_MINIMAL -DWINDOW_MINIMAL_NO_THREAD window-none.cpp -o retroprofile
+mv retroprofile ~/bin/retroprofiled
 
 <<COMMENT2
 #endif
 
-#include "minir.h"
+#include "../window.h"
+#include "../minir.h"
+#include "../file.h"
 #include <stdlib.h>
 #include <stdio.h>
 
-void no_audio(struct audio * This, unsigned int numframes, const int16_t * samples) {}
-int16_t no_input(struct libretroinput * This, unsigned port, unsigned device, unsigned index, unsigned id) { return 0; }
-
 int main(int argc, char * argv[])
 {
+	if (!argv[1])
+	{
+		puts("usage: retroprofile <core> <rom> <frames>");
+		return 1;
+	}
 	//window_init(&argc, &argv);
 	unsigned int frames=atoi(argv[3]);
 	
 	uint64_t t_init=window_get_time();
-	struct libretro * core=libretro_create(argv[1], NULL, NULL);
+	struct libretro * core=libretro::create(argv[1], NULL, NULL);
 	if (!core)
 	{
-		puts("Couldn't load core.");
+		perror(argv[1]);
+		//puts("Couldn't load core.");
 		return 1;
 	}
 	
-	video* v=list_video[0]->create2d(0);
-	
-	static struct audio noaudio;
-	noaudio.render=no_audio;
-	
-	static struct libretroinput noinput;
-	noinput.query=no_input;
-	
-	core->attach_interfaces(core, v, &noaudio, &noinput);
-	
-	if (!core->load_rom(core, NULL,0, argv[2]))
+	if (!core->load_rom(file::create(argv[2])))
 	{
+		perror(argv[2]);
 		puts("Couldn't load ROM.");
 		return 1;
 	}
@@ -56,17 +53,17 @@ int main(int argc, char * argv[])
 	unsigned int fps=0;
 	while (window_get_time() < t_run+1000000 && fps < frames)
 	{
-		core->run(core);
+		core->run();
 		fps++;
 	}
 	for (unsigned int i=fps;i<frames;i++)
 	{
 		if (i%fps==0) printf("%i/%i\r", i, frames),fflush(stdout);
-		core->run(core);
+		core->run();
 	}
 	uint64_t t_run_done=window_get_time();
 	
-	core->free(core);
+	delete core;
 	uint64_t t_end=window_get_time();
 	
 	printf("Time to load ROM: %luus\n", t_run-t_init);
--- window-none.cpp
+++ window-none.cpp
@@ -7,12 +7,7 @@
 
 #include <time.h>
 
-static void init_mutex_stuff();
-void window_init(int * argc, char * * argv[])
-{
-	_window_init_native();
-	init_mutex_stuff();
-}
+void window_init(int * argc, char * * argv[]) {}
 
 uint64_t window_get_time()
 {
@@ -21,60 +16,13 @@ uint64_t window_get_time()
 	return ts.tv_sec*1000000 + ts.tv_nsec/1000;
 }
 
-bool file_read(const char * filename, void* * data, size_t * len)
-{
-	FILE * file=fopen(filename, "rb");
-	if (!file) return false;
-	fseek(file, 0, SEEK_END);
-	size_t len_=ftell(file);
-	fseek(file, 0, SEEK_SET);
-	char* data_=malloc(len_+1);
-	size_t truelen=fread(data_, 1,len_, file);
-	fclose(file);
-	
-	if (len_!=truelen)
-	{
-		free(data_);
-		return false;
-	}
-	data_[len_]='\0';
-	*data=data_;
-	if (len) *len=len_;
-	return true;
-}
+file* file::create(const char * filename) { return create_fs(filename); }
 
-bool file_write(const char * filename, const char * data, size_t len)
-{
-	FILE * file=fopen(filename, "wb");
-	if (!file) return false;
-	size_t truelen=fwrite(data, 1,len, file);
-	return (len==truelen);
-}
-
-//bool file_read_to(const char * filename, char * data, size_t len)
-//{
-	//
-//}
-
-char * window_get_absolute_path(const char * basepath, const char * path, bool allow_up)
-{
-	return _window_native_get_absolute_path(basepath, path, allow_up);
-}
-
-#if defined(WINDOW_MINIMAL_IMUTEX)
-static mutex* imutex[_imutex_count];
-static void init_mutex_stuff()
-{
-	for (unsigned int i=0;i<_imutex_count;i++) imutex[i]=new mutex;
-}
-
-void _int_mutex_lock(enum _int_mutex id) { imutex[id]->lock(); }
-bool _int_mutex_try_lock(enum _int_mutex id) { return imutex[id]->try_lock(); }
-void _int_mutex_unlock(enum _int_mutex id) { imutex[id]->unlock(); }
-#else
-static void init_mutex_stuff() {}
-void _int_mutex_lock(enum _int_mutex id) {}
-bool _int_mutex_try_lock(enum _int_mutex id) {}
-void _int_mutex_unlock(enum _int_mutex id) {}
+#ifdef WINDOW_MINIMAL_NO_THREAD
+mutex* mutex::create() { return NULL; }
+void mutex::lock() {}
+bool mutex::try_lock() { return true; }
+void mutex::unlock() {}
+void mutex::release() {}
 #endif
 #endif
