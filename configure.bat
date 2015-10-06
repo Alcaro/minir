5f835cfd96e953493448af70ca98b9cdab89156c
Alcaro <floating@muncher.se>
2015-10-06 21:03:12 +0200

    Echoing all that just looks stupid. Also add a snarky remark.

--- configure.bat
+++ configure.bat
@@ -1,3 +1,7 @@
+@echo off
+
+echo Checking whether build environment is sane...
+
 echo WIN_LIB =  -lgdi32     > config.mk
 echo WIN_LIB += -lcomctl32 >> config.mk
 echo WIN_LIB += -lcomdlg32 >> config.mk
@@ -14,3 +18,6 @@ echo 	$(RC) $(RCFLAGS) ico/minir.rc obj/resource$(OBJSUFFIX).o >> config.mk
 
 ::some people just refuse to leave their XPs.
 ver | find "5.1" > nul && echo CONF_CFLAGS = -D_WIN32_WINNT=0x0501 >> config.mk
+
+ping -n2 127.0.0.1
+echo ...it's a Windows, so probably not, but configuration is done anyways.
